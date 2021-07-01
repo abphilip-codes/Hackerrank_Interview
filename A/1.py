d0a11ca88b357ce5adf988b87362b1fdd9899fe3
Allen <allenalvin333@gmail.com>
2021-07-01 07:13:32 +0530

    programs

--- A/1.py
+++ A/1.py
@@ -1,2 +1,39 @@
 # https://www.hackerrank.com/challenges/sock-merchant/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    ans = 0
+    for z in set(ar):
+        ans = ans + ar.count(z)//2
+    return ans
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
