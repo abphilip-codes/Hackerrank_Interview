13b71f9b05c2b372cacfd0903ba1ad80e882a64f
Allen <allenalvin333@gmail.com>
2021-07-01 13:14:37 +0530

    programs

---
+++ A/4.py
@@ -0,0 +1,34 @@
+# https://www.hackerrank.com/challenges/repeated-string/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    return s.count('a')*(n//len(s))+s[0:n%len(s)].count('a')
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
