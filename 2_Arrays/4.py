a060c45183640461707a6a6ebc74720e9ee1285b
allenalvin333 <allenalvin333@gmail.com>
2021-08-05 07:20:24 +0530

    programs

--- 2_Arrays/4.py
+++ 2_Arrays/4.py
@@ -1,2 +1,32 @@
 # https://www.hackerrank.com/challenges/minimum-swaps-2/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(A):
+    k=0
+    for i in range(len(A)):
+        m = A.index(min(A[i:])) 
+        if(m!=i):
+            A[i], A[m] = A[m], A[i]
+            k+=1
+    return k
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
