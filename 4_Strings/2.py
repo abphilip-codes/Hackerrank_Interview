e1793cf640375f7ed68e4df302d132d63bd7de7c
allenalvin333 <allenalvin333@gmail.com>
2021-08-16 21:46:43 +0530

    programs

---
+++ 4_Strings/2.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/alternating-characters/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    return len([z for z in range(len(s)-1) if(s[z]==s[z+1])])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
