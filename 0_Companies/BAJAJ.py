4decc1221ca668a268ae100f832a4bd408425ad6
allenalvin333 <allenalvin333@gmail.com>
2021-08-16 13:11:19 +0530

    programs

--- 0_Companies/BAJAJ.py
+++ 0_Companies/BAJAJ.py
@@ -5,6 +5,8 @@ import json
 import jsonpath
 
 endpoint = "https://reqres.in/api/users/2"
-endpoint="https://prod24.centralindia.logic.azure.com:443/workflows/78d6df0ed1384ee0b7d04918f1a32b85/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=i6gXuS7-5_fFVf-0u8M4UfymINDULCMifsscfN5cPKM"
-# Put request to endpoint 
+# endpoint="https://prod24.centralindia.logic.azure.com:443/workflows/78d6df0ed1384ee0b7d04918f1a32b85/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=i6gXuS7-5_fFVf-0u8M4UfymINDULCMifsscfN5cPKM"
+
+file = open("C:\Users\allen\OneDrive\Desktop\Github\Competitive\HackerRank\Interview\0_Companies\BAJAJ.json", "r")
+req = json.loads(file.read())
 r = requests.post(endpoint)
