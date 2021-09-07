7b9008b0671e9080121efe1d92ed8bc1f14b1175
allenalvin333 <allenalvin333@gmail.com>
2021-09-07 06:27:19 +0530

    programs

---
+++ 3_Sorting/2.py
@@ -0,0 +1,42 @@
+# https://www.hackerrank.com/challenges/mark-and-toys/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    c=s=0
+    prices.sort()
+    for z in range(len(prices)):
+        if(s<=k): s,c=s+prices[z],c+1
+    return c-1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
