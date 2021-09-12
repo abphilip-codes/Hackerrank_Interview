ba25cc02e0e439da8527e38445a358d6ad468bbc
allenalvin333 <allenalvin333@gmail.com>
2021-09-12 19:30:07 +0530

    programs

---
+++ 7_Greedy/2.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/luck-balance/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'luckBalance' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. 2D_INTEGER_ARRAY contests
+#
+
+def luckBalance(k, contests):
+    contests.sort(reverse=True)
+    ans = 0
+    for c in contests:
+        if (c[1]==0): ans+=c[0]
+        elif(k>0): ans,k=ans+c[0],k-1
+        else: ans-=c[0]
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
