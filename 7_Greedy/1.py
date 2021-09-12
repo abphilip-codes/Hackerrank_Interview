3b09854856605223646a578f6d49f7747224cc02
allenalvin333 <allenalvin333@gmail.com>
2021-09-12 19:27:39 +0530

    programs

---
+++ 7_Greedy/1.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    return min(abs(x-y) for x,y in zip(arr,arr[1:]))   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
