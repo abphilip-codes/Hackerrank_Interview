0d37d58297310f49a63cc65660806e8993cc0637
allenalvin333 <allenalvin333@gmail.com>
2021-09-15 17:08:21 +0530

    programs

--- 8_Searching/2.py
+++ 8_Searching/2.py
@@ -1,2 +1,41 @@
-# https://www.hackerrank.com/challenges/swap-nodes-algo/problem
+# https://www.hackerrank.com/challenges/pairs/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr):
+    d,ans = {z : 1 for i,z in enumerate(arr)},0
+    for z in d:
+        if((z+k) in d): ans+=1
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
