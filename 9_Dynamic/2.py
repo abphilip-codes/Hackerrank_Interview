322c8ddd8fa9c6287c294346897eef4d1cb9d264
allenalvin333 <allenalvin333@gmail.com>
2021-09-21 10:25:26 +0530

    programs

--- 9_Dynamic/2.py
+++ 9_Dynamic/2.py
@@ -18,18 +18,17 @@ import sys
 #
 
 def abbreviation(a, b):
-    m,n = len(a),len(b)
-    d = [[False]*(m+1) for _ in range(n+1)]
+    d = [[False]*(len(a)+1) for _ in range(len(b)+1)]
     d[0][0] = True
-    for z in range(n+1):
-        for y in range(m+1):
+    for z in range(len(b)+1):
+        for y in range(len(a)+1):
             if (z==0 and y!=0):
                 d[z][y] = a[y-1].islower() and d[z][y-1]
             elif (z!=0 and y!=0):
                 if (a[y-1] == b[z-1]): d[z][y] = d[z-1][y-1]
                 elif (a[y-1].upper() == b[z-1]): d[z][y] = d[z-1][y-1] or d[z][y-1]
                 elif not (a[y-1].isupper() and b[z-1].isupper()): d[z][y] = d[z][y-1]
-    return "YES" if (d[n][m]) else "NO"
+    return "YES" if (d[len(b)][len(a)]) else "NO"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
