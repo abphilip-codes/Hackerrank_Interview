03862a11988396edd8b859a6bb5653aa28f4b394
allenalvin333 <allenalvin333@gmail.com>
2021-09-22 07:40:27 +0530

    programs

--- 9_Dynamic/4.py
+++ 9_Dynamic/4.py
@@ -15,24 +15,25 @@ import sys
 # The function accepts LONG_INTEGER x as parameter.
 #
 
-def decibinaryNumbers(x):
+def decibinaryNumbers():
     d={}
-    for z in range(0,100000):
+    for z in range(0,10000000,10):
         n,r=str(z)[::-1],0
         for y in range(len(n)): r+=int(n[y])*(2**y)
         d.update({z:r})
     ans=sorted(d.items(),key=lambda i:(i[1],i[0]))
-    return(ans[x-1][0])        
-
+    return ans
+    
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     q = int(input().strip())
-
+    ans=decibinaryNumbers()
+    
     for q_itr in range(q):
         x = int(input().strip())
-
-        result = decibinaryNumbers(x)
+        
+        result = ans[x-1][0]
 
         fptr.write(str(result) + '\n')
 
