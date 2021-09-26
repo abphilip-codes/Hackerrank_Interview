7cca9c9a5d2e950616c79b2fdb5d3047e3ca5f22
allenalvin333 <allenalvin333@gmail.com>
2021-09-26 10:05:48 +0530

    programs

--- 10_Stacks_Queues/4.py
+++ 10_Stacks_Queues/4.py
@@ -10,12 +10,30 @@ import sys
 
 # Complete the riddle function below.
 def riddle(arr):
-    ans,n=[],len(arr)
-    for z in range(1,n+1):
-        temp=[]
-        for y in range(n-z+1): temp.append(min(arr[y:y+z]))
-        ans.append(max(temp))
-    return ans
+    # ans,n=[],len(arr)
+    # for z in range(1,n+1):
+    #     temp=[]
+    #     for y in range(n-z+1): temp.append(min(arr[y:y+z]))
+    #     ans.append(max(temp))
+    # return ans
+
+    stack = []
+    d = {}
+    # to pop all the remaining elements in the stack
+    tmp = arr[:] + [-float('inf')]
+    # we get how big is the window for which this item is min
+    for i in range(len(tmp)):
+        indexed = i
+        while stack and tmp[i] <= stack[-1][0]:
+            n, indexed = stack.pop()
+            d[i - indexed] = max(d.get(i - indexed,0), n)
+        stack.append((tmp[i], indexed))
+    # we get the max's per window size
+    res, current = [0]*len(arr), 0
+    for i in reversed(range(len(arr))):
+        current = max(d.get(i+1, current), current)
+        res[i] = current
+    return res
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
