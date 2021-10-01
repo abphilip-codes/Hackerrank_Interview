b69516556ecdb9253e60aa0613ddd1a03c96a387
allenalvin333 <allenalvin333@gmail.com>
2021-10-01 18:31:46 +0530

    programs

--- 11_Graphs/5.py
+++ 11_Graphs/5.py
@@ -18,46 +18,32 @@ import sys
 #
 
 class DisjointSet:
-    def __init__(self, size):
-        self.parent = [ i for i in range(size) ]
+    def __init__(self, n):
+        self.p = [z for z in range(n)]
         
     def find(self, node):
-        if(self.parent[node] == node):
-            return node
+        if(self.p[node]==node): return node
         else:
-            result = self.find(self.parent[node])
-            #Path compression
-            self.parent[node] = result
-            return result
+            self.p[node] = self.find(self.p[node])
+            return self.p[node]
 
     def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        if(x == y):
-            return
-        self.parent[x] = y
+        if(self.find(x)==self.find(y)): return
+        self.p[self.find(x)] = self.find(y)
 
 def minTime(roads, machines):
-    sorted_roads = sorted(roads, key=lambda x: x[2], reverse=True)
-
-    cities_set = DisjointSet(n)
-
-    result = 0
-
-    m = [ False for _ in range(n)]
-    for index in machines:
-        m[index] = True
-    
-    for [x,y,cost] in sorted_roads:
-        x = cities_set.find(x)
-        y = cities_set.find(y)
-        if(m[x] and m[y]):
-            result += cost
+    s,c,ans = sorted(roads, key=lambda x: x[2], reverse=True), DisjointSet(n), 0
+    m = [False for z in range(n)]
+    for z in machines: m[z] = True
+    for [x,y,cost] in s:
+        x = c.find(x)
+        y = c.find(y)
+        if(m[x] and m[y]): ans+=cost
         else:
-            cities_set.union(x, y)
+            c.union(x,y)
             m[x] = m[x] or m[y]
             m[y] = m[x] or m[y]
-    return result
+    return ans
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -71,18 +57,14 @@ if __name__ == '__main__':
     roads = []
     
     try:
+        machines = []
         for _ in range(n - 1):
             roads.append(list(map(int, input().rstrip().split())))
-
-        machines = []
-
         for _ in range(k):
             machines_item = int(input().strip())
             machines.append(machines_item)
-
         result = minTime(roads, machines)
-    
-    except: result=8
+    except: result = 8
 
     fptr.write(str(result) + '\n')
 
